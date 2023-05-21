95cb7d0236e0af4d415ffff3b0ae8670f70c58a1
KatzaCat <katzacat1@gmail.com>
2023-05-21 16:55:23 -0500

    simple types

---
+++ include/KCtype.hpp
@@ -0,0 +1,25 @@
+#ifndef KCTYPE
+#define KCTYPE
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include <stdint.h>
+
+#include "KCtuple.hpp"
+
+typedef float    f32;
+typedef double   f64;
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+
+typedef std::string str;
+
+#endif // KCTYPE
