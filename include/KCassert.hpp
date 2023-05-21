58646bcffb938f960e7795a24693a521082007a8
KatzaCat <katzacat1@gmail.com>
2023-05-21 14:40:15 -0500

    changed assert macros to move to next line

--- include/KCassert.hpp
+++ include/KCassert.hpp
@@ -6,23 +6,23 @@
 
 #define KC_ASSERT(exp, msg) \
     if (!(exp))             \
-        {std::cout << "in " << __FILE__ << " " << __LINE__ << ": " << #exp << "\n\t" << msg << std::endl; exit(-1);}
+        {std::cout << "in " << __FILE__ << " " << __LINE__ << ": " << #exp << "\n\t" << msg << "\n" exit(-1);}
 
 #define KC_ASSERT_WITHOUT_QUIT(exp, msg) \
     if (!(exp))                          \
-        {std::cout << "in " << __FILE__ << " " << __LINE__ << ": " << #exp << "\n\t" << msg << std::endl;}
+        {std::cout << "in " << __FILE__ << " " << __LINE__ << ": " << #exp << "\n\t" << msg << "\n";}
 
-#define KC_ASSERT_TO_FILE(exp, msg)                                                                \
-    if (!(exp)) {                                                                                  \
-        std::ofstream ofs("./ASSERT.txt", std::ios::out);                                          \
-        ofs << "in " << __FILE__ << " " << __LINE__ << ": " << #exp << "\n\t" << msg << std::endl; \
-        exit(-1);                                                                                  \
+#define KC_ASSERT_TO_FILE(exp, msg)                                                           \
+    if (!(exp)) {                                                                             \
+        std::ofstream ofs("./ASSERT.txt", std::ios::out);                                     \
+        ofs << "in " << __FILE__ << " " << __LINE__ << ": " << #exp << "\n\t" << msg << "\n"; \
+        exit(-1);                                                                             \
     }
     
-#define KC_ASSERT_TO_FILE_WITHOUT_QUIT(exp, msg)                                                   \
-    if (!(exp)) {                                                                                  \
-        std::ofstream ofs("./ASSERT.txt", std::ios::out);                                          \
-        ofs << "in " << __FILE__ << " " << __LINE__ << ": " << #exp << "\n\t" << msg << std::endl; \
+#define KC_ASSERT_TO_FILE_WITHOUT_QUIT(exp, msg)                                              \
+    if (!(exp)) {                                                                             \
+        std::ofstream ofs("./ASSERT.txt", std::ios::out);                                     \
+        ofs << "in " << __FILE__ << " " << __LINE__ << ": " << #exp << "\n\t" << msg << "\n"; \
     }
 
 #endif
